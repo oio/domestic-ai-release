060fda040722a620aa3d94ec2832dd2e343bfda5
sblbl <marta@oio.studio>
2025-05-26 16:56:12 +0200

    fixes, frontend

--- domestic-ui/src/routes/+page.svelte
+++ domestic-ui/src/routes/+page.svelte
@@ -1,26 +1,40 @@
 <script>
-	import { onMount } from 'svelte';
+	import { onMount } from 'svelte'
+	let responseChat = $state(''),
+		responseImagen = $state('')
 
 	const submitChat = async (event) => {
-		event.preventDefault();
+		event.preventDefault()
+		responseChat = ''
 		const prompt = event.target.prompt.value;
 		const response = await fetch('/api/query', {
 			method: 'POST',
-			body: JSON.stringify({ message: prompt })
+			body: JSON.stringify({ prompt })
 		});
-		const data = await response.json();
-		console.log(data);
+		const data = await response.json()
+		if (response.ok) {
+			console.log(data)
+			responseChat = data.result
+		} else {
+			console.error(data)
+		}
 	};
 
 	const submitImagen = async (event) => {
 		event.preventDefault();
+		responseImagen = ''
 		const prompt = event.target.prompt.value;
 		const response = await fetch('/api/imagen', {
 			method: 'POST',
-			body: JSON.stringify({ message: prompt })
+			body: JSON.stringify({ prompt })
 		});
 		const data = await response.json();
-		console.log(data);
+		if (response.ok) {
+			console.log(data);
+			responseImagen = 'data:image/png;base64,' + data.result.b64;
+		} else {
+			console.error(data);
+		}
 	};
 
 	onMount(async () => {
@@ -28,23 +42,33 @@
 			method: 'POST',
 			body: JSON.stringify({})
 		});
-		const data = await response.json();
-		console.log(data);
+		const data = await response.json()
+		if (response.ok) {
+			console.log(data)
+		} else {
+			console.error(data)
+		}
 	});
 </script>
 
 <section>
 	<h1>Domestic AI</h1>
-	<form on:submit|preventDefault={submitChat}>
+	<form onsubmit={submitChat}>
 		<h2>Chat</h2>
 		<label for="prompt">Prompt</label>
 		<input type="text" id="prompt" name="prompt" value="What's a tayaki" />
 		<button type="submit">Send</button>
+		{#if responseChat}
+			<p>{responseChat}</p>
+		{/if}
 	</form>
-	<form on:submit|preventDefault={submitImagen}>
+	<form onsubmit={submitImagen}>
 		<h2>Imagen</h2>
 		<label for="prompt">Prompt</label>
 		<input type="text" id="prompt" name="prompt" value="A tayaki" />
 		<button type="submit">Send</button>
+		{#if responseImagen}
+			<img src={responseImagen} alt="Imagen" class="h-24" />
+		{/if}
 	</form>
 </section>
